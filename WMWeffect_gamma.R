b4e1c853ca0adbd1c9f9b9198117d70eaca7da47
statkm <kmizuma0215@gmail.com>
2024-07-07 14:00:34 +0900

    WMWeffect revised

--- WMWeffect_gamma.R
+++ WMWeffect_gamma.R
@@ -3,36 +3,6 @@ library(tidyverse)
 library(ggplot2)
 
 
-# debag
-
-# solving
-k1<- 1; k2 <- 3; 
-
-K<-k1+k2
-coef_ <- numeric(K)
-coef_k1 <- choose(k1, 0:k1)*((-1)^(0:k1))/gamma(k1) 
-coef_k1
-
-n<-0
-for(n in 0:(k2-1))
-coef_[(n+1):(n+1+length(coef_k1)-1)] <- 
-  coef_[(n+1):(n+1+length(coef_k1)-1)]+gamma(k1+n)/gamma(n+1)*coef_k1
-
-coef_[1]<-coef_[1]-0.5
-coef_
-
-roots<-polyroot(coef_)
-
-root<-roots[(abs(Im(roots)) < 10^-6) & (Re(roots)>0)]
-length(root)
-
-Re(root)
-
-
-
-
-
-
 func_Erlang_WMW <- function(k1, k2, eps=10^-6){
   K<-k1+k2
   coef_ <- numeric(K)
@@ -58,16 +28,51 @@ lambda1 <- 1-results$r_root;
 lambda2 <- results$r_root;
 
 
+
+
 # checking via Simulation
 N<-10000000
 dfx <- rgamma(N, shape=k1, rate = lambda1)
 dfy <- rgamma(N, shape=k2, rate = lambda2)
-mean(dfx<dfy)
+mean(dfx<dfy) #P(X<Y)
 
-
+#plot of density
 xmax <- max(k1/lambda1+sqrt(k1/(lambda1^2))*2,
             k2/lambda2+sqrt(k2/(lambda2^2))*2)
 ggplot(data = data.frame(X=c(0, xmax)), aes(x=X)) +
   stat_function(fun=function(x) dgamma(x, shape=k1, rate = lambda1), aes(color="X")) +
   stat_function(fun=function(x) dgamma(x, shape=k2, rate = lambda2), aes(color="Y"))
+
+
+
+
+
+
+
+
+# debag
+
+# solving
+k1<- 1; k2 <- 3; 
+
+K<-k1+k2
+coef_ <- numeric(K)
+coef_k1 <- choose(k1, 0:k1)*((-1)^(0:k1))/gamma(k1) 
+coef_k1
+
+n<-0
+for(n in 0:(k2-1))
+  coef_[(n+1):(n+1+length(coef_k1)-1)] <- 
+  coef_[(n+1):(n+1+length(coef_k1)-1)]+gamma(k1+n)/gamma(n+1)*coef_k1
+
+coef_[1]<-coef_[1]-0.5
+coef_
+
+roots<-polyroot(coef_)
+
+root<-roots[(abs(Im(roots)) < 10^-6) & (Re(roots)>0)]
+length(root)
+
+Re(root)
+
 
